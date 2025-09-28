db8441403e124b6667531563ff3661b1d2c20320
fangyong <351075478@qq.com>
2025-09-28 16:53:57 +0800

    feat: 简化项目

---
+++ docs/assets/static/chunks/pages/_app-c2bb61da43b75770.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{3965:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(2443)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(3965),_(7254)}),_N_E=n.O()}]);
